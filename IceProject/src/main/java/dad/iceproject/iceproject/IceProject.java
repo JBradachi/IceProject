2df0c94efd2434091359a66d1f314911922c8db6
João Bradachi <joao.bradachi@ufv.br>
2024-04-15 17:21:00 -0300

    add no git

---
+++ IceProject/src/main/java/dad/iceproject/iceproject/IceProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dad.iceproject.iceproject;
+
+/**
+ *
+ * @author Gordo
+ */
+public class IceProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
