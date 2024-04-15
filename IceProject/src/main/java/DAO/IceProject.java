8ddfe19d159436c2c913c9671f4f6827db76927f
João Bradachi <joao.bradachi@ufv.br>
2024-04-15 17:27:56 -0300

    Update IceProject.java

--- IceProject/src/main/java/DAO/IceProject.java
+++ IceProject/src/main/java/DAO/IceProject.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package DAO;
 
-/**
- *
- * @author Gordo
- */
 public class IceProject {
 
     public static void main(String[] args) {
